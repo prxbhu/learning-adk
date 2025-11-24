c6264459e5aa9d3248a645b65c483356d7037764
Vignesh <prxbhu@gmail.com>
2025-11-24 19:11:21 +0530

    mcp img gen agent

---
+++ image_agent/agent.py
@@ -0,0 +1,43 @@
+import uuid
+from google.genai import types
+
+from google.adk.agents import Agent
+from google.adk.models.google_llm import Gemini
+from google.adk.runners import Runner
+from google.adk.sessions import InMemorySessionService
+
+from google.adk.tools.mcp_tool.mcp_toolset import McpToolset
+from google.adk.tools.tool_context import ToolContext
+from google.adk.tools.mcp_tool.mcp_session_manager import StdioConnectionParams
+from mcp import StdioServerParameters
+
+from google.adk.apps.app import App
+from google.adk.tools.function_tool import FunctionTool
+
+retry_config = types.HttpRetryOptions(
+    attempts=5,  # Maximum retry attempts
+    exp_base=7,  # Delay multiplier
+    initial_delay=1,
+    http_status_codes=[429, 500, 503, 504],  # Retry on these HTTP errors
+)
+
+mcp_image_server = McpToolset(
+    connection_params=StdioConnectionParams(
+        server_params=StdioServerParameters(
+            command="npx",  # Run MCP server via npx
+            args=[
+                "-y",  # Argument for npx to auto-confirm install
+                "@modelcontextprotocol/server-everything",
+            ],
+            tool_filter=["getTinyImage"],
+        ),
+        timeout=30,
+    )
+)
+
+root_agent = Agent(
+    model=Gemini(model="gemini-2.5-flash-lite", retry_options=retry_config),
+    name="root_agent",
+    instruction="Use the MCP Tool to generate images for user queries",
+    tools=[mcp_image_server],
+)
